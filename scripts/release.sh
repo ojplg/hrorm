62ba6fa10ef8cb6e9c9a9a871583dd6fa7b41dea
ojplg <lees@ripco.com>
2018-10-08 12:16:04 -0500

    ordering

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,9 +15,9 @@ function printstep {
 }
 
 printstep "Correct version number in pom.xml"
+printstep "Update latest version maven link on website"
 printstep "Create tag in git"
 printstep "Run mvn deploy (requires gpg passphrase) to publish to oss.sonatype.org"
-printstep "Update latest version maven link on website"
 printstep "Push artifact from oss.sonatype.org to maven central (requires login)"
 printstep "Put jar file artifacts on downloads page https://github.com/ojplg/hrorm/releases"
 printstep "Update website on hrorm.org (aws)"
