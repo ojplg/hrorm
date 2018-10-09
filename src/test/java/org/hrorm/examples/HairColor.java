2869d19746a863d9f467e928f772f9d07de5918c
ojplg <lees@ripco.com>
2018-10-09 00:21:14 -0500

    More docs

--- src/test/java/org/hrorm/examples/HairColor.java
+++ src/test/java/org/hrorm/examples/HairColor.java
@@ -1,4 +1,12 @@
 package org.hrorm.examples;
 
 public class HairColor {
+
+    public static HairColor forColorName(String s) {
+        return null;
+    }
+
+    public String getColorName(){
+        return "";
+    }
 }
--- src/test/java/org/hrorm/examples/Person.java
+++ src/test/java/org/hrorm/examples/Person.java
@@ -1,9 +1,13 @@
 package org.hrorm.examples;
 
 import lombok.Data;
+import org.hrorm.Converter;
+import org.hrorm.Dao;
 import org.hrorm.DaoBuilder;
+import org.mockito.Mockito;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
 import java.time.LocalDateTime;
 
 @Data
@@ -18,9 +22,49 @@ class Person {
 
     private void builderDemo() {
         DaoBuilder<Person> daoBuilder = new DaoBuilder<>("PERSON_TABLE", Person::new);
+
         daoBuilder.withPrimaryKey("ID","PERSON_SEQUENCE", Person::getId, Person::setId);
+
+        daoBuilder.withStringColumn("NAME", Person::getName, Person::setName);
+
+        daoBuilder.withIntegerColumn("WEIGHT", Person::getWeight, Person::setWeight);
+
+        daoBuilder.withBigDecimalColumn("HEIGHT", Person::getHeight, Person::setHeight);
+
+        daoBuilder.withLocalDateTimeColumn("BIRTHDAY", Person::getBirthday, Person::setBirthday);
+
+        daoBuilder.withBooleanColumn("IS_HIGH_SCHOOL_GRADUATE", Person::isHighSchoolGraduate, Person::setHighSchoolGraduate);
+
+        daoBuilder.withConvertingStringColumn("HAIR_COLOR", Person::getHairColor, Person::setHairColor, new HairColorConverter());
     }
 
+    private void fluidDemo(){
+        DaoBuilder<Person> daoBuilder = new DaoBuilder<>("PERSON_TABLE", Person::new)
+                .withPrimaryKey("ID","PERSON_SEQUENCE", Person::getId, Person::setId)
+                .withStringColumn("NAME", Person::getName, Person::setName)
+                .withIntegerColumn("WEIGHT", Person::getWeight, Person::setWeight)
+                .withBigDecimalColumn("HEIGHT", Person::getHeight, Person::setHeight)
+                .withLocalDateTimeColumn("BIRTHDAY", Person::getBirthday, Person::setBirthday)
+                .withBooleanColumn("IS_HIGH_SCHOOL_GRADUATE", Person::isHighSchoolGraduate, Person::setHighSchoolGraduate)
+                .withConvertingStringColumn("HAIR_COLOR", Person::getHairColor, Person::setHairColor, new HairColorConverter());
+
+        Connection connection = Mockito.mock(Connection.class);
+
+        Dao<Person> personDao = daoBuilder.buildDao(connection);
+
+    }
+}
+
+class HairColorConverter implements Converter<HairColor, String> {
+    @Override
+    public String from(HairColor item) {
+        return item.getColorName();
+    }
+
+    @Override
+    public HairColor to(String s) {
+        return HairColor.forColorName(s);
+    }
 }
 
 
