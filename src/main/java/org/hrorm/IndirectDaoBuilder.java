a5dc3618b6dd9d661eec59528b02536fefbde0af
ojplg <lees@ripco.com>
2018-10-17 17:10:13 -0500

    clean up

--- src/main/java/org/hrorm/IndirectDaoBuilder.java
+++ src/main/java/org/hrorm/IndirectDaoBuilder.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.sql.Connection;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
