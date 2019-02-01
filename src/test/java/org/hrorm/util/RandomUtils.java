83a05b40e01360fd08f2579efa96ccc5e8ea5968
Scott Serbousek <airetamstrm@gmail.com>
2019-01-31 21:24:40 -0500

    RandomUtils comments

--- src/test/java/org/hrorm/util/RandomUtils.java
+++ src/test/java/org/hrorm/util/RandomUtils.java
@@ -20,22 +20,42 @@ import java.util.stream.Stream;
 public class RandomUtils {
     private static final Random RANDOM = new Random(System.currentTimeMillis());
 
-
-
     private RandomUtils() {
         // Don't construct me!
     }
 
+    /**
+     * Calls endlessSupplier.get between min and max times to produce that many E's.
+     * @param min The minimum number of items
+     * @param max The maximum number of items
+     * @param endlessSupplier The supplier of items. Should be able to call this indefinitely.
+     * @param <E> The type of thing returned.
+     * @return Returns a random sized List of E, with between min and max number of elements.
+     */
     public static <E> List<E> randomNumberOf(int min, int max, Supplier<E> endlessSupplier) {
         return IntStream.range(0, range(min, max))
                 .mapToObj(i -> endlessSupplier.get())
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Picks a random element out of a list.
+     * @param list The Dataset to select from.
+     * @param <E> The Type of entity in the list, and the type of entity returned.
+     * @return A random element from in list.
+     */
     public static <E> E randomMemberOf(List<E> list) {
         return list.get(RANDOM.nextInt(list.size()));
     }
 
+    /**
+     * Produces a random subset of list, all containing the same value from getter::apply
+     * @param list The Dataset to select from.
+     * @param getter The Getter method on E to invoke.
+     * @param <E> The Entity Type.
+     * @param <F> The Field Type returned by getter::apply.
+     * @return A sub selection of List, all with the same value provided by the field getter.
+     */
     public static <E, F> List<E> randomSubsetByField(List<E> list, Function<E, F> getter) {
         List<F> possibleValues = list.stream()
                 .map(getter)
@@ -86,27 +106,46 @@ public class RandomUtils {
                         ));
     }
 
+    /**
+     * Produce an integer with a highest and lowest possible value.
+     * @param min Lowest number we should expect
+     * @param max Highest number we should expect.
+     */
     public static int range(int min, int max) {
         return RANDOM.nextInt(Math.abs(max-min))+min;
     }
 
+    /**
+     * Random boolean value.
+     */
     public static boolean bool() {
         return RANDOM.nextBoolean();
     }
 
+    /**
+     * Random BigDecimal. Based on double input.
+     */
     public static BigDecimal bigDecimal() {
         return BigDecimal.valueOf(RANDOM.nextDouble());
     }
 
+    /**
+     * Select one of the statically-defined names below.
+     */
     public static String name() {
         return randomMemberOf(NAMES);
     }
 
+    /**
+     * A little more unique than name, but still human-readable.
+     */
     public static String biname() {
         return name()+"-"+name();
     }
 
-    // Something human readable for debugging
+    /**
+     * Names to select from.
+     */
     private static final List<String> NAMES = Stream.of(
         "Donut",
         "Penguin",
