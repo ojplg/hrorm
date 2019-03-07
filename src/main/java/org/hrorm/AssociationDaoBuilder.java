dcb4e9f22d22057f978b2b5d71fba58f8828c715
ojplg <lees@ripco.com>
2019-03-07 08:02:58 -0600

    Simplify code using composition

--- src/main/java/org/hrorm/AssociationDaoBuilder.java
+++ src/main/java/org/hrorm/AssociationDaoBuilder.java
@@ -4,53 +4,27 @@ import java.sql.Connection;
 
 public class AssociationDaoBuilder<LEFT, RIGHT> {
 
-    private final String tableName;
-    private final String primaryKeyName;
-    private final String sequenceName;
-
-    private String leftColumnName;
-    private DaoDescriptor<LEFT, LEFT> leftDaoDescriptor;
-
-    private String rightColumnName;
-    private DaoDescriptor<RIGHT, RIGHT> rightDaoDescriptor;
+    private final IndirectAssociationDaoBuilder<LEFT, LEFT, RIGHT, RIGHT> indirectDaoBuilder;
 
     public AssociationDaoBuilder(String tableName,
                                  String primaryKeyName,
                                  String sequenceName) {
-        this.tableName = tableName;
-        this.primaryKeyName = primaryKeyName;
-        this.sequenceName = sequenceName;
+        indirectDaoBuilder = new IndirectAssociationDaoBuilder<>(tableName, primaryKeyName, sequenceName);
     }
 
     public AssociationDao<LEFT, RIGHT> buildDao(Connection connection){
-        DaoBuilder<Association<LEFT, RIGHT>> internalDaoBuilder =
-                new DaoBuilder<Association<LEFT, RIGHT>>(tableName, Association::new)
-                        .withPrimaryKey(primaryKeyName, sequenceName, Association::getId, Association::setId)
-                        .withJoinColumn(leftColumnName, Association::getLeft, Association::setLeft, leftDaoDescriptor)
-                        .withJoinColumn(rightColumnName, Association::getRight, Association::setRight, rightDaoDescriptor);
-
-        Dao<Association<LEFT, RIGHT>> internalDao = internalDaoBuilder.buildDao(connection);
-
-        return new AssociationDaoImpl<>(
-                internalDao,
-                leftColumnName,
-                rightColumnName,
-                leftDaoDescriptor.primaryKey(),
-                rightDaoDescriptor.primaryKey()
-        );
+        return indirectDaoBuilder.buildDao(connection);
     }
 
     public AssociationDaoBuilder<LEFT, RIGHT> withLeft(String leftColumnName,
                                                        DaoDescriptor<LEFT, LEFT> leftDaoDescriptor){
-        this.leftColumnName = leftColumnName;
-        this.leftDaoDescriptor = leftDaoDescriptor;
+        this.indirectDaoBuilder.withLeft(leftColumnName, leftDaoDescriptor);
         return this;
     }
 
     public AssociationDaoBuilder<LEFT, RIGHT> withRight(String rightColumnName,
                                                         DaoDescriptor<RIGHT, RIGHT> rightDaoDescriptor){
-        this.rightColumnName = rightColumnName;
-        this.rightDaoDescriptor = rightDaoDescriptor;
+        this.indirectDaoBuilder.withRight(rightColumnName, rightDaoDescriptor);
         return this;
     }
 
