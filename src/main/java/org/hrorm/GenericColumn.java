88db6bc0535a9d0839a5f22ed7457c102e054282
ojplg <lees@ripco.com>
2019-03-29 12:04:57 -0500

    Docs

--- src/main/java/org/hrorm/GenericColumn.java
+++ src/main/java/org/hrorm/GenericColumn.java
@@ -4,11 +4,39 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * A column that represents a particular Java type.
+ *
+ * <p>
+ *     This can be used if none of the types Hrorm has built in meet
+ *     the needs. The user must provide mechanisms for setting the
+ *     type's value onto a <code>java.sql.PreparedStatement</code> and
+ *     for reading a value from a <code>java.sql.ResultSet</code>.
+ * </p>
+ *
+ * <p>
+ *      To create a column for <code>Integer</code>, do the following:
+ * </p>
+ *
+ * <pre>{@code
+ * GenericColumn<Integer> integerColumn = new GenericColumn<>(
+ *     PreparedStatement::setInt,
+ *     ResultSet::getInt);
+ * }</pre>
+ *
+ * @param <TYPE> The Java type represented by the column.
+ */
 public class GenericColumn<TYPE> {
 
-    private final int sqlType;
+    private final Integer sqlType;
     private final PreparedStatementSetter<TYPE> preparedStatementSetter;
     private final ResultSetReader<TYPE> resultReader;
+
+    public GenericColumn(PreparedStatementSetter<TYPE> preparedStatementSetter, ResultSetReader<TYPE> resultReader){
+        this.sqlType = null;
+        this.preparedStatementSetter = preparedStatementSetter;
+        this.resultReader = resultReader;
+    }
 
     public GenericColumn(PreparedStatementSetter<TYPE> preparedStatementSetter, ResultSetReader<TYPE> resultReader, int sqlType){
         this.sqlType = sqlType;
