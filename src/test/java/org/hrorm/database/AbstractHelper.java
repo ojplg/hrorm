353a36b206d73b3c07b08fdbbaa52ac122119def
ojplg <lees@ripco.com>
2019-03-29 20:48:28 -0500

    Clean up imports

--- src/test/java/org/hrorm/database/AbstractHelper.java
+++ src/test/java/org/hrorm/database/AbstractHelper.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
