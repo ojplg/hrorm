12acf0e325c23a47bec8e2bda24a71cb320ebb34
ojplg <lees@ripco.com>
2019-03-29 21:08:37 -0500

    Fix some bugs with null column handling

--- src/main/java/org/hrorm/DataColumnFactory.java
+++ src/main/java/org/hrorm/DataColumnFactory.java
@@ -87,7 +87,11 @@ public class DataColumnFactory {
         return new AbstractColumn<Boolean, ENTITY, BUILDER>(name, prefix, getter, setter, nullable) {
             @Override
             public Boolean fromResultSet(ResultSet resultSet, String columnName) throws SQLException {
-                return resultSet.getBoolean(columnName);
+                Boolean result = resultSet.getBoolean(columnName);
+                if ( resultSet.wasNull() ){
+                    return null;
+                }
+                return result;
             }
 
             @Override
@@ -236,7 +240,7 @@ public class DataColumnFactory {
             @Override
             public E fromResultSet(ResultSet resultSet, String columnName) throws SQLException {
                 Long code = resultSet.getLong(columnName);
-                if ( code == null ) {
+                if ( resultSet.wasNull() ) {
                     return null;
                 }
                 return converter.to(code);
