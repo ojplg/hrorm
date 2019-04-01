16cbe11330fc07b5be4944cbaf6bd7ba2a66b261
ojplg <lees@ripco.com>
2019-04-01 16:12:54 -0500

    Script up postgres testing

--- src/test/java/org/hrorm/database/HelperFactory.java
+++ src/test/java/org/hrorm/database/HelperFactory.java
@@ -1,10 +1,21 @@
 package org.hrorm.database;
 
+import static org.hrorm.database.HelperFactory.DatabasePlatform.H2;
+import static org.hrorm.database.HelperFactory.DatabasePlatform.Postgres;
+
 public class HelperFactory {
 
+    enum DatabasePlatform { H2, Postgres }
+
+    public static DatabasePlatform CURRENT_PLATFORM = H2;
+
     public static Helper forSchema(String schemaName){
-        return h2Helper(schemaName);
-//        return postgresHelper(schemaName);
+        if( CURRENT_PLATFORM == H2 ) {
+            return h2Helper(schemaName);
+        } else if (CURRENT_PLATFORM == Postgres) {
+            return postgresHelper(schemaName);
+        }
+        throw new RuntimeException("What plaform is this: " + CURRENT_PLATFORM);
     }
 
     public static Helper h2Helper(String schemaName){
