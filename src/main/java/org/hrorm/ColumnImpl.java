249df36472ffd29a184304aef16d68cfd8c9e91f
ojplg <lees@ripco.com>
2019-04-12 17:42:53 -0500

    Simplify.

---
+++ src/main/java/org/hrorm/ColumnImpl.java
@@ -0,0 +1,141 @@
+package org.hrorm;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+public class ColumnImpl<TYPE, ENTITY, BUILDER> implements Column<ENTITY,BUILDER> {
+
+    private final GenericColumn<TYPE> genericColumn;
+
+    private final String name;
+    private final String prefix;
+    private final Function<ENTITY, TYPE> getter;
+    private final BiConsumer<BUILDER, TYPE> setter;
+
+    private String sqlTypeName;
+    private boolean nullable;
+
+    public ColumnImpl(GenericColumn<TYPE> genericColumn,
+                      String prefix,
+                      String name,
+                      Function<ENTITY, TYPE> getter,
+                      BiConsumer<BUILDER, TYPE> setter,
+                      String sqlTypeName,
+                      boolean nullable){
+        this.genericColumn = genericColumn;
+
+        this.prefix = prefix;
+        this.name = name;
+        this.setter = setter;
+        this.getter = getter;
+        this.sqlTypeName = sqlTypeName;
+        this.nullable = nullable;
+    }
+
+    public <MODELTYPE> ColumnImpl(GenericColumn<TYPE> genericColumn,
+                               String prefix,
+                               String name,
+                               Function<ENTITY, MODELTYPE> getter,
+                               BiConsumer<BUILDER, MODELTYPE> setter,
+                               String sqlTypeName,
+                               boolean nullable,
+                               Converter<MODELTYPE, TYPE> converter){
+        this.genericColumn = genericColumn;
+
+        this.prefix = prefix;
+        this.name = name;
+        this.setter = convertedSetter(converter, setter);
+        this.getter = convertedGetter(converter, getter);
+        this.sqlTypeName = sqlTypeName;
+        this.nullable = nullable;
+    }
+
+    private static <T, E, DB> Function<E, DB> convertedGetter(Converter<T, DB> converter, Function<E, T> rawGetter){
+        return entity -> {
+            T value = rawGetter.apply(entity);
+            if( value == null ){
+                return null;
+            }
+            return converter.from(value);
+        };
+    }
+
+    private static <T, B, DB> BiConsumer<B, DB> convertedSetter(Converter<T, DB> converter, BiConsumer<B, T> setter){
+        return (builder, dbType) -> {
+            if ( dbType != null ){
+                T value = converter.to(dbType);
+                setter.accept(builder, value);
+            }
+        };
+    }
+
+    @Override
+    public void setValue(ENTITY item, int index, PreparedStatement preparedStatement) throws SQLException {
+        TYPE value = getter.apply(item);
+        if ( value == null && ! nullable ){
+            throw new HrormException("Tried to set a null value for " + prefix + "." + name + " which was set not nullable.");
+        }
+        if ( value == null ) {
+            preparedStatement.setNull(index,genericColumn.sqlType());
+        } else {
+            genericColumn.setPreparedStatement(preparedStatement, index, value);
+        }
+    }
+
+    @Override
+    public PopulateResult populate(BUILDER builder, ResultSet resultSet) throws SQLException {
+        TYPE value = genericColumn.fromResultSet(resultSet, prefix  + name);
+        setter.accept(builder, value);
+        return PopulateResult.Ignore;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getPrefix() {
+        return prefix;
+    }
+
+    @Override
+    public void notNull() {
+        nullable = false;
+    }
+
+    @Override
+    public boolean isNullable() {
+        return nullable;
+    }
+
+    @Override
+    public Set<Integer> supportedTypes() {
+        return genericColumn.getSupportedTypes();
+    }
+
+    @Override
+    public String getSqlTypeName() {
+        return sqlTypeName;
+    }
+
+    @Override
+    public void setSqlTypeName(String sqlTypeName) {
+        this.sqlTypeName = sqlTypeName;
+    }
+
+    @Override
+    public Column<ENTITY, BUILDER> withPrefix(String newPrefix, Prefixer prefixer) {
+        return new ColumnImpl(this.genericColumn,
+                newPrefix,
+                this.name,
+                this.getter,
+                this.setter,
+                this.sqlTypeName,
+                this.nullable);
+    }
+}
--- src/main/java/org/hrorm/DataColumnFactory.java
+++ src/main/java/org/hrorm/DataColumnFactory.java
@@ -16,56 +16,55 @@ public class DataColumnFactory {
 
     public static <ENTITY, BUILDER> Column<ENTITY, BUILDER> bigDecimalColumn(
             String name, String prefix, Function<ENTITY, BigDecimal> getter, BiConsumer<BUILDER, BigDecimal> setter, boolean nullable) {
-        return new SimpleColumnImpl<>(GenericColumn.BIG_DECIMAL, prefix, name, getter, setter, GenericColumn.BIG_DECIMAL.getSqlTypeName(), nullable);
+        return new ColumnImpl<>(GenericColumn.BIG_DECIMAL, prefix, name, getter, setter, GenericColumn.BIG_DECIMAL.getSqlTypeName(), nullable);
     }
 
     public static <ENTITY, BUILDER> Column<ENTITY, BUILDER> longColumn(
             String name, String prefix, Function<ENTITY, Long> getter, BiConsumer<BUILDER, Long> setter, boolean nullable) {
-        return new SimpleColumnImpl<>(GenericColumn.LONG, prefix, name, getter, setter, GenericColumn.LONG.getSqlTypeName(), nullable);
+        return new ColumnImpl<>(GenericColumn.LONG, prefix, name, getter, setter, GenericColumn.LONG.getSqlTypeName(), nullable);
     }
 
     public static <ENTITY, BUILDER> Column<ENTITY, BUILDER> booleanColumn(
             String name, String prefix, Function<ENTITY, Boolean> getter, BiConsumer<BUILDER, Boolean> setter, boolean nullable) {
-        return new SimpleColumnImpl<>(GenericColumn.BOOLEAN, prefix, name, getter, setter, GenericColumn.BOOLEAN.getSqlTypeName(), nullable);
+        return new ColumnImpl<>(GenericColumn.BOOLEAN, prefix, name, getter, setter, GenericColumn.BOOLEAN.getSqlTypeName(), nullable);
     }
 
     public static <ENTITY, BUILDER> Column<ENTITY, BUILDER> textBackedBooleanColumn(
             String name, String prefix, Function<ENTITY, Boolean> getter, BiConsumer<BUILDER, Boolean> setter, boolean nullable) {
-        return new ConvertingColumnImpl<>(GenericColumn.STRING, prefix, name, getter, setter, GenericColumn.STRING.getSqlTypeName(),nullable, Converters.T_F_BOOLEAN_STRING_CONVERTER);
+        return new ColumnImpl<>(GenericColumn.STRING, prefix, name, getter, setter, GenericColumn.STRING.getSqlTypeName(),nullable, Converters.T_F_BOOLEAN_STRING_CONVERTER);
     }
 
     public static <ENTITY, BUILDER> Column<ENTITY, BUILDER> longBackedBooleanColumn(
             String name, String prefix, Function<ENTITY, Boolean> getter, BiConsumer<BUILDER, Boolean> setter, boolean nullable) {
-        return new ConvertingColumnImpl<>(GenericColumn.LONG, prefix, name, getter, setter, GenericColumn.LONG.getSqlTypeName(),nullable, Converters.ONE_ZERO_BOOLEAN_LONG_CONVERTER);
+        return new ColumnImpl<>(GenericColumn.LONG, prefix, name, getter, setter, GenericColumn.LONG.getSqlTypeName(),nullable, Converters.ONE_ZERO_BOOLEAN_LONG_CONVERTER);
     }
 
     public static <ENTITY, BUILDER> Column<ENTITY, BUILDER> stringColumn(
             String name, String prefix, Function<ENTITY, String> getter, BiConsumer<BUILDER, String> setter, boolean nullable) {
-        return new SimpleColumnImpl<>(GenericColumn.STRING, prefix, name, getter, setter, GenericColumn.STRING.getSqlTypeName(), nullable);
+        return new ColumnImpl<>(GenericColumn.STRING, prefix, name, getter, setter, GenericColumn.STRING.getSqlTypeName(), nullable);
     }
 
     public static <ENTITY, BUILDER> Column<ENTITY, BUILDER> instantColumn(
             String name, String prefix, Function<ENTITY, Instant> getter, BiConsumer<BUILDER, Instant> setter, boolean nullable) {
-        return new ConvertingColumnImpl<>(GenericColumn.TIMESTAMP, prefix, name, getter, setter, GenericColumn.TIMESTAMP.getSqlTypeName(), nullable, Converters.INSTANT_TIMESTAMP_CONVERTER);
+        return new ColumnImpl<>(GenericColumn.TIMESTAMP, prefix, name, getter, setter, GenericColumn.TIMESTAMP.getSqlTypeName(), nullable, Converters.INSTANT_TIMESTAMP_CONVERTER);
     }
-
 
     public static <TYPE, ENTITY, BUILDER> Column<ENTITY, BUILDER> stringConverterColumn(
             String name, String prefix, Function<ENTITY, TYPE> getter, BiConsumer<BUILDER, TYPE> setter,
             Converter<TYPE, String> converter, boolean nullable) {
-        return new ConvertingColumnImpl<>(GenericColumn.STRING, prefix, name, getter, setter, GenericColumn.STRING.getSqlTypeName(), nullable, converter);
+        return new ColumnImpl<>(GenericColumn.STRING, prefix, name, getter, setter, GenericColumn.STRING.getSqlTypeName(), nullable, converter);
     }
 
     public static <TYPE, ENTITY, BUILDER> Column<ENTITY, BUILDER> genericColumn(
             String name, String prefix, Function<ENTITY, TYPE> getter, BiConsumer<BUILDER, TYPE> setter,
             GenericColumn<TYPE> genericColumn, boolean nullable) {
-        return new SimpleColumnImpl<>(genericColumn, prefix, name, getter, setter, genericColumn.getSqlTypeName(), nullable);
+        return new ColumnImpl<>(genericColumn, prefix, name, getter, setter, genericColumn.getSqlTypeName(), nullable);
     }
 
     public static <TYPE, DBTYPE, ENTITY, BUILDER> Column<ENTITY, BUILDER> convertedGenericColumn(
             String name, String prefix, Function<ENTITY, TYPE> getter, BiConsumer<BUILDER, TYPE> setter,
             GenericColumn<DBTYPE> genericColumn, Converter<TYPE, DBTYPE> converter, boolean nullable) {
-        return new ConvertingColumnImpl<TYPE, DBTYPE, ENTITY, BUILDER>(genericColumn, prefix, name, getter, setter, genericColumn.getSqlTypeName(), nullable, converter);
+        return new ColumnImpl<>(genericColumn, prefix, name, getter, setter, genericColumn.getSqlTypeName(), nullable, converter);
     }
 
 }
