f4c036c2cde67653d9d52562b65dbc18b2832453
ojplg <lees@ripco.com>
2019-10-03 22:16:39 -0400

    New populate result with joined item

--- src/main/java/org/hrorm/PopulateResult.java
+++ src/main/java/org/hrorm/PopulateResult.java
@@ -1,5 +1,7 @@
 package org.hrorm;
 
+import com.sun.scenario.effect.impl.prism.ps.PPSBlend_ADDPeer;
+
 import java.sql.Connection;
 import java.util.function.Consumer;
 
@@ -11,6 +13,9 @@ import java.util.function.Consumer;
  * Most users of hrorm will have no need to directly use this.
  */
 public class PopulateResult {
+
+    public static final String JoinedItemResult = "Joined Item Result";
+
     public static final PopulateResult PrimaryKey = new PopulateResult("Primary Key");
     public static final PopulateResult NoPrimaryKey = new PopulateResult("No Primary Key");
     public static final PopulateResult Ignore = new PopulateResult("Ignore");
@@ -18,23 +23,40 @@ public class PopulateResult {
 
     private final String code;
     private final Consumer<Connection> connectionUser;
+    private final Object joinedItem;
 
     private PopulateResult(String code){
         this.code = code;
         this.connectionUser = con -> {};
+        this.joinedItem = null;
     }
 
     private PopulateResult(Consumer<Connection> connectionUser){
         this.connectionUser = connectionUser;
         this.code = "Join Column";
+        this.joinedItem = null;
+    }
+
+    private PopulateResult(Object object){
+        this.code = JoinedItemResult;
+        this.connectionUser = con -> {};
+        this.joinedItem = object;
     }
 
     public static PopulateResult fromJoinColumn(Consumer<Connection> connectionUser){
         return new PopulateResult(connectionUser);
     }
 
+    public static PopulateResult fromJoinColumn(Object item){
+        return new PopulateResult(item);
+    }
+
     public void populateChildren(Connection connection){
         this.connectionUser.accept(connection);
+    }
+
+    public boolean isJoinedItemResult(){
+        return code.equals(JoinedItemResult);
     }
 
     @Override
